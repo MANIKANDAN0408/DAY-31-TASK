caab353c8b0ec33f5721f77abab6bbd6102cc0aa
MANIKANDAN0408 <115692293+MANIKANDAN0408@users.noreply.github.com>
2023-02-21 13:25:11 +0530

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Navbar from './navbar';
+
+const Home = () => {
+    return (
+        <div className='h'>
+            <Navbar/>
+            <h1 className='home'>📔STUDENT TEACHER MANAGEMENT🏫</h1>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/navbar.js
@@ -0,0 +1,22 @@
+
+import React from "react"
+import { Link } from "react-router-dom"
+ 
+function Navbar(){
+    return(
+        <>
+        <nav className="nav-bar">
+           
+            <Link to="/" className="student"> 🏠HOME</Link>
+
+            <Link to={"/student"} className="student">👨‍🎓STUDENT</Link>
+
+            <Link to={"/teacher"} className="student">🧑‍🏫TEACHER</Link>
+          
+               
+         
+        </nav>
+        </>
+    )
+}
+export default Navbar
